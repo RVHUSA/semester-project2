50e48b38df8db686353efd0cc25e49574ef5edd3
RVHUSA <rebecca.husa@hotmail.com>
2026-03-25 20:04:36 +0100

    Add API key and comments in config.js
    Includes;
    - API key for authenticated requests
    - Comments explaining code

--- api/config.js
+++ api/config.js
@@ -1,1 +1,5 @@
+// Base URL for the Noroff API
 export const apiBaseUrl = "https://v2.api.noroff.dev";
+
+// API key used for authenticated requests
+export const apiKey = "6c53d5ea-690b-42eb-97ab-1fd861ce25a9";
